f2d1a10688e1e17b475f688920ccaa7114cc6d2c
Scerocraw <Scerocraw@elitecoder.eu>
2020-01-06 11:17:33 +0100

    Update index.php

--- index.php
+++ index.php
@@ -2,10 +2,16 @@
 
 require_once __DIR__ . '/sequence.php';
 
+// Controls the input / sequence to extend
 $input    = [ 9, 73, 241, 561, 1081, 1849 ];
+
+// By how many numbers the sequence should be extended
 $extendBy = 2;
+
+// Stuff
 $extender = new extender();
 $result   = $extender->proceed($input, $extendBy);
 
+// Output
 echo "<pre>";
 var_dump("RESULT:", $input, $result);
